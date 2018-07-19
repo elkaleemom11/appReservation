6c7cd73121ae5b4ab523a07210ed05b21596adb0
Connie <elkaleemom11@gmail.com>
2018-07-19 13:21:02 -0600

    edits to html and js files

--- main/server.js
+++ main/server.js
@@ -1,18 +1,11 @@
 // Dependencies
 // =============================================================
-var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
+var express = require("express");
 var path = require("path");
+
+var app = express();
 var PORT = process.env.PORT || 8080;
-
-// Sets up the Express App
-// =============================================================
-app.get('/', function (req, res) {
-    res.send("hello world");
-})
-
-
 
 // parse app/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,11 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-    res.setHeader("Content-Type", "text/plain")
-    res.write("you posted:\n")
-    res.end(JSON.stringify(req.body, nul, 2))
-})
+
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
